16113b4527d06320813f3be4efb1ff0c2b021621
kiran paturi <kpaturi@smarttuition.com>
2016-06-27 15:09:09 +0530

    added code

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -4,4 +4,23 @@ angular.module('register')
         $scope.registerUser = function () {
             console.log($scope.user);
         };
+        $scope.countries = [{
+                "name": "India",
+                "code": "IN",
+                "ISDCode": "+91",
+                "continent": "Asia"
+            },
+            {
+                "name": "Canada",
+                "code": "CA",
+                "ISDCode": "+45",
+                "continent": "North America"
+            },
+            {
+                "name": "United States",
+                "code": "US",
+                "continent": "America",
+                ISDCode: "+1"
+            }];
+
     });
