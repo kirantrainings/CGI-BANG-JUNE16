ecf225946267d687714405774b16b93ffcd1d326
kiran paturi <kpaturi@smarttuition.com>
2016-06-29 10:25:00 +0530

    added latest codeR

---
+++ app/register/registerFact.js
@@ -0,0 +1,17 @@
+angular.module('register')
+    .factory('registerFact', function () {
+        return {
+            saveUser: function (user {
+                console.log("user Saved");
+            });
+        }
+    });
+
+
+angular.module('register')
+    .service('registerService', function () {
+            this.saveUser: function (user {
+                console.log("user Saved");
+            });
+        };
+    });
